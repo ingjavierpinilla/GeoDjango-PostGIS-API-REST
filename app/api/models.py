ad3f3ceb41c78fb72edafbcadf2fa8d26669db4e
Javier <ing.jmpinillag@gmail.com>
2021-02-03 10:15:06 +0100

    campo nombre slug para dataset

--- app/api/models.py
+++ app/api/models.py
@@ -1,15 +1,20 @@
 from datetime import datetime, timezone
-from django.db import models
+from django.db import models, migrations
 from django.contrib.postgres.operations import CreateExtension
-from django.db import migrations
+from django.utils.text import slugify
 from django.contrib.gis.db import models as gis_models
 
 class Dataset(models.Model):
     name = models.CharField(max_length = 95, blank = False)
     date = models.DateTimeField(default = datetime.now(timezone.utc), blank = False)
+    slug = models.SlugField(max_length = 95, blank = True , null = True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Dataset, self).save(*args, **kwargs)
 
     def __str__(self):
-        return f'{self.id} {self.name}'
+        return f'{self.id} {self.slug}'
 
 class Row(models.Model):
     dataset_id = models.ForeignKey(Dataset, related_name='rows', on_delete = models.CASCADE)
